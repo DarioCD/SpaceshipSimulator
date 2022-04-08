4b6d357181b455eedee615ea2a373e163f2312ce
Darío Chinea Delgado <dariocd0808@gmail.com>
2022-04-08 12:54:21 +0100

    Feature: Añadido valores a introducir por el usuario
    Se añadio el nombre, matricula y aceleración de la nave espacial

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce el nombre de la nave: ");
+        String nombre = sc.nextLine();
+        System.out.print("Introduce la matrícula de la nave: ");
+        String matricula = sc.nextLine();
+        System.out.print("Introduce la aceleración de la nave: ");
+        float aceleracion = sc.nextFloat();
+        SpaceShip nave = new SpaceShip(nombre, matricula, aceleracion);
     }
 }
